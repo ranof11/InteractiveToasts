91228eae2e502e9a6865322e2749b77c60707b0e
ranof11 <rajeshtriadi14@gmail.com>
2025-05-18 14:42:47 +0700

    feat: interactive toast

--- InteractiveToasts/ContentView.swift
+++ InteractiveToasts/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toasts: [Toast] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Text("Dummy list row")
+            }
+            .navigationTitle("Toasts")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Show") {
+                        showToast()
+                    }
+                }
+            }
         }
-        .padding()
+        .interactiveToasts($toasts)
+    }
+    
+    func showToast() {
+        withAnimation(.bouncy) {
+            let toast = Toast { id in
+                ToastView(id)
+            }
+            
+            toasts.append(toast)
+        }
+    }
+    
+    // Custom Toast View
+    @ViewBuilder
+    func ToastView(_ id: String) -> some View {
+        HStack(spacing: 12) {
+            Image(systemName: "airpods.pro")
+            
+            Text("Jes' Airpods")
+                .font(.callout)
+            
+//            Spacer(minLength: 0)
+//            
+//            Button {
+//                $toasts.delete(id)
+//            } label: {
+//                Image(systemName: "xmark.circle.fill")
+//                    .font(.title2)
+//            }
+
+        }
+        .foregroundStyle(.primary)
+        .padding(.vertical, 12)
+        .padding(.horizontal, 15)
+//        .padding(.trailing, 10)
+        .background {
+            Capsule()
+                .fill(.background)
+                .shadow(color: .black.opacity(0.06), radius: 3, x: 1, y: -3)
+                .shadow(color: .black.opacity(0.06), radius: 2, x: 1, y: 3)
+        }
+//        .padding(.horizontal)
     }
 }
 
