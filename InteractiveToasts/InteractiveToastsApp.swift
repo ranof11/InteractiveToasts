47ad22d7360661c73eabc89aac41e7173cbc91b4
ranof11 <rajeshtriadi14@gmail.com>
2025-05-18 13:30:25 +0700

    Initial Commit

---
+++ InteractiveToasts/InteractiveToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveToastsApp.swift
+//  InteractiveToasts
+//
+//  Created by Rajesh Triadi Noftarizal on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
